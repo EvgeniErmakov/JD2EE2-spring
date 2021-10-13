347932e38ce43e87f589ae77225f37ea063d160b
Evgeni Ermakov <evgeni_ermakov@Mail.ru>
2021-10-13 11:28:43 +0300

    refactoring: add constants instead of String-parameters

--- src/main/java/by/it/academy/news/command/MainPageCommand.java
+++ src/main/java/by/it/academy/news/command/MainPageCommand.java
@@ -27,20 +27,23 @@ public class MainPageCommand {
     private static final String ALL_NEWS_ATTRIBUTE = "allNews";
     private static final String PAGE_COUNT_ATTRIBUTE = "pageCount";
     private static final String UPDATE_FAILED_MESSAGE = "Update failed";
-    private static final String NEWS_WAS_UPDATED_MESSAGE = "News was updated!";
-    private static final String NEWS_WAS_PUBLISHED_MESSAGE = "News was published!";
+    private static final String NEWS_WAS_UPDATED_MESSAGE = "News has been updated!";
+    private static final String NEWS_WAS_PUBLISHED_MESSAGE = "News has was published!";
+    private static final String NEWS_WAS_DELETED_MESSAGE = "News has deleted!";
     private static final String CURRENT_PAGE_NUMBER_ATTRIBUTE = "currentPageNumber";
     private static final String GO_TO_MAIN_PAGE = "main-page";
     private static final String GO_TO_UPDATE_PAGE = "update-page";
+    private static final String GO_TO_LOGIN_PAGE = "login-page";
     private static final String GO_TO_SINGLE_NEWS_PAGE = "single-news-page";
     private static final String GO_TO_CREATE_NEWS_PAGE = "create-news-page";
     private static final String GO_TO_OFFER_NEWS_PAGE = "offer-news-page";
     private static final String GO_TO_OFFERED_NEWS_PAGE = "offered-news-page";
     private static final String CSS_ATTRIBUTE = "css";
     private static final String REDIRECT_NEWS_OFFERED_PAGE = "redirect:/news/toOfferedNewsPage?page=";
+    private static final String REDIRECT_NEWS_UPDATE_PAGE = "redirect:/news/showUpdatePage?id=";
     private static final String REDIRECT_NEWS_START_PAGE = "redirect:/news/start?page=";
     private static final String MSG_ATTRIBUTE = "msg";
-    private static final String CSS_DANGER_VALUE = "danger";
+    private static final String CSS_DANGER_VALUE = "Warning!";
     private static final String CSS_SUCCESS_VALUE = "Success";
 
     @Autowired
@@ -67,7 +70,6 @@ public class MainPageCommand {
         return GO_TO_MAIN_PAGE;
     }
 
-
     @RequestMapping("/toOfferedNewsPage")
     public String toOfferedNewsPage(@RequestParam(name = "page", defaultValue = "1") int page, final Model model,
                                     final HttpSession httpSession) throws NewsServiceException {
@@ -79,7 +81,6 @@ public class MainPageCommand {
         model.addAttribute(ALL_NEWS_ATTRIBUTE, allNews);
         model.addAttribute(PAGE_COUNT_ATTRIBUTE, pageCount);
         httpSession.setAttribute(CURRENT_PAGE_NUMBER_ATTRIBUTE, page);
-        System.out.println("offered");
         return GO_TO_OFFERED_NEWS_PAGE;
     }
 
@@ -87,10 +88,8 @@ public class MainPageCommand {
     public String toSingleNewsPage(@PathVariable(PATH_VARIABLE_ID) int id, final Model model) throws NewsServiceException {
         final News singleNews = newsService.getSingleNews(id);
         model.addAttribute(NEWS_ATTRIBUTE, singleNews);
-
         return GO_TO_SINGLE_NEWS_PAGE;
     }
-
 
     @RequestMapping("/toAddNewsPage")
     public String toAddNewsPage(Model model) throws NewsServiceException {
@@ -108,9 +107,8 @@ public class MainPageCommand {
 
     @GetMapping("/login")
     public String login() {
-        return "login";
+        return GO_TO_LOGIN_PAGE;
     }
-
 
     @PostMapping("/addNews")
     public ModelAndView addNews(@Valid @ModelAttribute("news") News news, BindingResult theBindingResult) throws NewsServiceException {
@@ -119,7 +117,7 @@ public class MainPageCommand {
             modelAndView.setViewName(GO_TO_CREATE_NEWS_PAGE);
         } else {
             newsService.addNews(news);
-            modelAndView.setViewName("redirect:/news/start");
+            modelAndView.setViewName(REDIRECT_NEWS_START_PAGE);
         }
         return modelAndView;
     }
@@ -131,7 +129,7 @@ public class MainPageCommand {
             modelAndView.setViewName(GO_TO_OFFER_NEWS_PAGE);
         } else {
             newsService.offerNews(news);
-            modelAndView.setViewName("redirect:/news/start");
+            modelAndView.setViewName(REDIRECT_NEWS_START_PAGE);
         }
         return modelAndView;
     }
@@ -157,7 +155,7 @@ public class MainPageCommand {
             redirectAttributes.addFlashAttribute(NEWS_ATTRIBUTE, news);
             redirectAttributes.addFlashAttribute(CSS_ATTRIBUTE, CSS_DANGER_VALUE);
             redirectAttributes.addFlashAttribute(MSG_ATTRIBUTE, UPDATE_FAILED_MESSAGE);
-            return "redirect:/news/showUpdatePage?id=" + id;
+            return REDIRECT_NEWS_UPDATE_PAGE + id;
         } else {
             newsService.updateNews(news);
             redirectAttributes.addFlashAttribute(CSS_ATTRIBUTE, CSS_SUCCESS_VALUE);
@@ -169,15 +167,12 @@ public class MainPageCommand {
         }
     }
 
-
     @PutMapping("/publish/{id}")
     public String publishNews(@PathVariable(PATH_VARIABLE_ID) int id, final HttpSession httpSession,
                               @Valid @ModelAttribute(NEWS_ATTRIBUTE) final News news, final BindingResult bindingResult,
                               final RedirectAttributes redirectAttributes)
             throws NewsServiceException {
-        System.out.println("работает");
         newsService.publishNews(id);
-        System.out.println("работает2");
         redirectAttributes.addFlashAttribute(CSS_ATTRIBUTE, CSS_SUCCESS_VALUE);
         redirectAttributes.addFlashAttribute(MSG_ATTRIBUTE, NEWS_WAS_PUBLISHED_MESSAGE);
 
@@ -187,14 +182,13 @@ public class MainPageCommand {
 
     }
 
-
     @DeleteMapping("/{id}")
     public String deleteNews(@PathVariable(PATH_VARIABLE_ID) int id, final RedirectAttributes redirectAttributes,
                              final HttpSession httpSession) throws NewsServiceException {
         newsService.deleteNews(id);
 
         redirectAttributes.addFlashAttribute(CSS_ATTRIBUTE, CSS_SUCCESS_VALUE);
-        redirectAttributes.addFlashAttribute(MSG_ATTRIBUTE, "News has deleted!");
+        redirectAttributes.addFlashAttribute(MSG_ATTRIBUTE, NEWS_WAS_DELETED_MESSAGE);
 
         int currentPage = (int) httpSession.getAttribute(CURRENT_PAGE_NUMBER_ATTRIBUTE);
 
